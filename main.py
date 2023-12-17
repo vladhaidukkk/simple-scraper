891bab5b11067c4d08309a6316dd97dfeb6b4cd3
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2023-12-17 16:37:08 +0200

    extract param values

--- main.py
+++ main.py
@@ -7,13 +7,33 @@ def get_tag_content(html: str, tag: str) -> str | None:
     return match.groups()[0] if match else None
 
 
+def get_param_value(html: str, param: str) -> str | None:
+    param_start = html.find(param)
+    if param_start < 0:
+        return None
+
+    relative_param_end = html[param_start:].find(":")
+    if relative_param_end < 0:
+        return None
+    param_end = param_start + relative_param_end
+
+    value_start = param_end + 1
+    value_end = value_start + html[value_start:].find("<")
+    value = html[value_start:] if value_end < 0 else html[value_start:value_end]
+    return value.strip()
+
+
 def main() -> None:
-    url = "http://olympus.realpython.org/profiles/poseidon"
+    url = "http://olympus.realpython.org/profiles/dionysus"
     response = urlopen(url)
     html = response.read().decode("utf-8")
 
     title = get_tag_content(html, "title")
-    print(title)
+    print(f"TITLE: {title}")
+
+    for param in ("Name", "Hometown", "Favorite animal", "Favorite Color"):
+        value = get_param_value(html, param)
+        print(f"{param.upper()}: {value}")
 
 
 if __name__ == "__main__":
