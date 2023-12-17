6842f54d5a91063b52c8c0a08dfbe1a91b6720a6
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2023-12-17 17:12:32 +0200

    use bs4 htmk parser

--- main.py
+++ main.py
@@ -1,39 +1,22 @@
-import re
 from urllib.request import urlopen
 
-
-def get_tag_content(html: str, tag: str) -> str | None:
-    match = re.search(rf"<{tag}.*?>(.*?)</{tag}.*?>", html, re.IGNORECASE)
-    return match.groups()[0] if match else None
-
-
-def get_param_value(html: str, param: str) -> str | None:
-    param_start = html.find(param)
-    if param_start < 0:
-        return None
-
-    relative_param_end = html[param_start:].find(":")
-    if relative_param_end < 0:
-        return None
-    param_end = param_start + relative_param_end
-
-    value_start = param_end + 1
-    value_end = value_start + html[value_start:].find("<")
-    value = html[value_start:] if value_end < 0 else html[value_start:value_end]
-    return value.strip()
+from bs4 import BeautifulSoup
 
 
 def main() -> None:
     url = "http://olympus.realpython.org/profiles/dionysus"
     response = urlopen(url)
     html = response.read().decode("utf-8")
+    soup = BeautifulSoup(html, "html.parser")
 
-    title = get_tag_content(html, "title")
-    print(f"TITLE: {title}")
+    if soup.title:
+        print(f"TITLE: {soup.title.string}")
 
-    for param in ("Name", "Hometown", "Favorite animal", "Favorite Color"):
-        value = get_param_value(html, param)
-        print(f"{param.upper()}: {value}")
+    body = soup.find("body")
+    if body:
+        for line in filter(None, body.get_text().splitlines()):
+            param, value = line.split(": ")
+            print(f"{param.upper()}: {value}")
 
 
 if __name__ == "__main__":
