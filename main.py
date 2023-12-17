bd5bcb854712c3f931c270c9c26791d20476559a
Vladyslav Haiduk <the.vladhaiduk@gmail.com>
2023-12-17 14:32:58 +0200

    get title text

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 from urllib.request import urlopen
 
-url = "http://olympus.realpython.org/profiles/aphrodite"
-response = urlopen(url)
-html = response.read().decode("utf-8")
 
-print(html)
+def get_tag_content(html: str, tag: str) -> str:
+    tag_start = html.index(f"<{tag}")
+    start = tag_start + html[tag_start:].index(">") + 1
+    end = html.index(f"</{tag}")
+    return html[start:end]
+
+
+def main() -> None:
+    url = "http://olympus.realpython.org/profiles/poseidon"
+    response = urlopen(url)
+    html = response.read().decode("utf-8")
+
+    title = get_tag_content(html, "title")
+    print(title)
+
+
+if __name__ == "__main__":
+    main()
